be84ede581456e65b2f9078b8384a13b7ad41b97
KetiPilishvili <52124129+KetiPilishvili@users.noreply.github.com>
2021-04-10 17:06:52 +0400

    Populate tag items, add menu hide / show method, add current menu item active method

--- js/library-click.js
+++ js/library-click.js
@@ -2,6 +2,9 @@
 
 let libClick = document.getElementById("lib-click");
 let menuItemTag = document.getElementById("menu-item-tag");
+let tags = document.getElementById("menu-content").getElementsByClassName("tag-space")
+let menuContent = document.getElementById("library-content");
+
 menuItemTag.addEventListener("click", handleMenuItemTagClick);
 libClick.addEventListener("click", libFunc);
 
@@ -19,44 +22,96 @@ function libFunc(addContent) {
     //get filter-items
     let filterElem = document.querySelectorAll('.filter-items');
 
-
     //get col classes
     let colChilds = document.getElementsByClassName("col");
 
     if (libListener.style.display == "block") {
         libListener.style.display = "none";
-        libClick.style.borderLeft =  "1px solid #707070";
-        for(var i=1; i<filterElem.length; i++)  {
+        libClick.style.borderLeft = "1px solid #707070";
+        for (var i = 1; i < filterElem.length; i++) {
             filterElem[i].style.color = "#fff";
         }
-        for (var a=0; a<colChilds.length; a+=4) {
+        for (var a = 0; a < colChilds.length; a += 4) {
             colChilds[a].style.display = "block";
         }
 
     } else {
         libListener.style.display = "block";
-        libClick.style.borderLeft =  "none";
-        for(var i=1; i<filterElem.length; i++)  {
+        libClick.style.borderLeft = "none";
+        for (var i = 1; i < filterElem.length; i++) {
             filterElem[i].style.color = "#333333";
         }
 
-        for (var a=0; a<colChilds.length; a+=4) {
+        for (var a = 0; a < colChilds.length; a += 4) {
             colChilds[a].style.display = "none";
         }
     }
 }
 
-function handleMenuItemTagClick(addContent){
-    let libListener = document.getElementById("library-content");
-    let colChilds = document.getElementsByClassName("col");
-    libListener.style.display = "block";
-   let labels = document.getElementById("menu-content").getElementsByClassName("checkbox");
-
-    for (var i=0; i<labels.length; i++) {
-            labels[i].style.display = "none";
-        }
-   
+// set default values
+for (var i = 0; i < tags.length; i++) {
+    tags[i].style.display = "none";
 }
+
+function handleMenuItemTagClick() {
+    openMenuContainer()
+    activeMenuItem(1);
+
+    for (var i = 0; i < tags.length; i++) {
+        tags[i].style.display = "block";
+    }
+}
+
+function openMenuContainer() {
+    // show menu container
+    let menuContent = document.getElementById("library-content");
+    menuContent.style.display = "block";
+
+    // hide labels - library menu items
+    let labels = document.getElementById("menu-content").getElementsByClassName("checkbox");
+    for (var i = 0; i < labels.length; i++) {
+        labels[i].style.display = "none";
+    }
+
+    // hide tags
+    let tags = document.getElementById("menu-content").getElementsByClassName("tag-space");
+    for (var i = 0; i < tags.length; i++) {
+        tags[i].style.display = "none";
+    }
+}
+
+function activeMenuItem(index) {
+    let menuItems = document.getElementsByClassName("filter-items");
+    for (i = 0; i < menuItems.length; i++) {
+        if (i == index) {
+            menuItems[i].style.color = "#fff";
+        } else {
+            menuItems[i].style.color = "#333333";
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
